8319b484fe9cd7bed01f8d3893f08c4ff7dc3bf9
Chad Sheets <cjsheets@gmail.com>
2017-10-30 19:56:28 -0800

    dash to underscore

---
+++ Makefile
@@ -0,0 +1,19 @@
+VERSION=`python setup.py --version`
+
+default:
+	@echo "Specify one of: serve, publish_docs, publish_package"
+
+env:
+	virtualenv .env
+
+serve:
+	.env/bin/mkdocs serve -s -a 0.0.0.0:8000
+
+publish_docs:
+	.env/bin/mkdocs gh-deploy
+
+publish_package:
+	@echo Build python distribution
+	python setup.py sdist bdist_wheel
+	@echo "Publish to PyPI at https://pypi.python.org/pypi/rtd-dropdown"
+	@echo "Run this manually: .env/bin/twine upload dist/rtd-dropdown-$(VERSION).tar.gz dist/rtd_dropdown-$(VERSION)-py2-none-any.whl"
--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@ from setuptools import setup, find_packages
 
 VERSION = '0.0.1'
 
-
 setup(
     name="rtd-dropdown",
     version=VERSION,
-    url='https://github.com/cjsheets/readthedocs-dropdown',
+    url='https://github.com/cjsheets/mkdocs-rtd-dropdown',
     license='MIT',
-    description='Minimal theme for MkDocs',
+    description='Clone of ReadTheDocs',
     author='Chad Sheets',
     author_email='chad@cjsheets.com',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
         'mkdocs.themes': [
-            'dropdown = rtd-dropdown',
+            'dropdown = rtd_dropdown',
         ]
     },
     zip_safe=False
